582d8a1315dea664f25f94f5e96c05921e4e8484
Bikesh Bimali <bimalibikesh@gmail.com>
2024-01-13 00:56:03 +0545

    add: create login and signup page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-import fs from "fs";
+const express = require("express");
+const PORT = process.env.PORT || 8000;
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs"); //middleware
+
+app.get("/", (req: any, res: any) => {
+  res.render("index");
+});
+
+app.get("/profile", (req: any, res: any) => {
+  res.render("profile", { user: "Bikesh" });
+});
+
+app.get("/users/signup", (req: any, res: any) => {
+  res.render("signup");
+});
+
+app.get("/users/login", (req: any, res: any) => {
+  res.render("login");
+});
+
+app.listen(PORT, () => {
+  console.log(`now listening on port ${PORT}`);
+});
