f55498c849382e227332aa4e285475d77f1aa16d
Bikesh Bimali <bimalibikesh@gmail.com>
2024-01-14 12:01:19 +0545

    fix: pass username  to the chat dashboard

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -3,7 +3,9 @@ import { Request, Response } from "express";
 
 export const dashboardController = {
   showDashboard(req: Request, res: Response) {
-    const user = req.user;
-    res.render("dashboard", { user: "Billllli" });
+    const user = req.user as { username?: string }; // Type assertion
+    console.log("See here-----------", user);
+    const username = user?.username || "Guest";
+    res.render("dashboard", { user: username });
   },
 };
